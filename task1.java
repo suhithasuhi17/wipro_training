c0813f4a4820703f3d7f7bc1ffb363f4d719a918
suhitha <157236546+suhithasuhi17@users.noreply.github.com>
2025-07-18 14:04:20 +0530

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+
+public class task1 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String a = sc.nextLine();
+    String b = sc.nextLine();
+    System.out.print(a + " Technologies " + b);
+}
+}
---
+++ task2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a);
+    }
+}
